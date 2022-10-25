0287b3e767e668e35a64ec155fc86dc0c51c255f
oskis123 <oskis2000@me.com>
2022-10-25 11:53:57 +0200

    endring 1

---
+++ year_test.py
@@ -0,0 +1,19 @@
+import leap_year
+
+
+def test_if_divisible_by_4():
+    assert leap_year.is_leap_year(4)
+
+
+def test_if_divisible_by_400():
+    assert leap_year.is_leap_year(400)
+
+
+def test_if_leap_year():
+    assert leap_year.is_leap_year(2000)
+
+
+def test_if_not_leap_year():
+    assert not leap_year.is_leap_year(1001)
+    assert not leap_year.is_leap_year(1500)
+    assert not leap_year.is_leap_year(350)
